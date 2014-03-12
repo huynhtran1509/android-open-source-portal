243ffa744a00f45fcae77d3fb507595c34f80f80
Derek Brameyer <derek.brameyer@willowtreeapps.com>
2014-03-12 10:43:15 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+android-open-source-portal
+==============
+
+Directory of open source Android libraries and software by WillowTree Apps.
---
+++ script.js
@@ -0,0 +1,40 @@
+var repos = [
+				"OAK",
+  				"lint-trap",
+   				"brewthumbsup-android",
+   				"maven-testflight-plugin",
+   				"gradle-testflight-plugin",
+   				"ci-teamcity-testflight-plugin",
+   			].sort();
+var repoTemplate;
+
+$(document).ready(function() {
+	compileTemplates();
+	displayPlaceholder();
+	loadRepos();
+});
+
+function loadRepos() {
+	repos.forEach(function (value, index) {
+		getRepo(value, function(data) {
+			$("#repos li:eq(" + index + ")").replaceWith(repoTemplate(data));
+		});
+	});
+}
+
+function displayPlaceholder() {
+	repos.forEach(function (value) {
+		var context = {name: value};
+		$("#repos").append(repoTemplate(context));
+	});
+}
+
+function compileTemplates() {
+	repoTemplate = Handlebars.compile($("#repo-template").html());
+}
+
+function getRepo(repoName, completion) {
+	$.getJSON("https://api.github.com/repos/willowtreeapps/" + repoName, function(data) {
+ 		completion(data);
+	});
+}
